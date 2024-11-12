07a6e4f61e1907cce23ceeeb98a7b2134ec2306d
SyutkinMO <syutkin.mo@gmail.com>
2024-11-12 10:31:01 +0500

    1_2_

--- module_9_4.py
+++ module_9_4.py
@@ -1,19 +1,22 @@
 # ------------Создание функций на лету------------
 
+"""Необходимо составить lambda-функцию для следующего выражения - list(map(?, first, second)).
+Здесь ? - место написания lambda-функции.
+Результатом должен быть список совпадения букв в той же позиции"""
 
 first = 'Мама мыла раму'
 second = 'Рамена мало было'
 
+list_bool = list(map(lambda x, y: x == y, first, second))
+print(list_bool)  # в данном случае будут учитываться пробелы, и сравнение по наиболее короткой строке
+
 from itertools import zip_longest
 
-print(list(
-    map(lambda x, y: list(i == j for i, j in (zip_longest(x.replace(' ', ''), y.replace(' ', '')))), first, second)))
-
 itog = list((x == y) for x, y in (zip_longest(first.replace(' ', ''), second.replace(' ', ''))))
-print(itog)
+print(itog)  # здесь выполнено удаление пробелов и недостающие буквы в более короткой строке будут заполнены None
 
 
-def func_(fir, sec):
+def func_(fir, sec):  # вариант в виде функции
     list_ = []
     for x, y in zip_longest(fir.replace(' ', ''), sec.replace(' ', '')):
         list_.append(x == y)
@@ -35,8 +38,21 @@ def get_advanced_writer(file_name):
     return write_everything
 
 
+"""Замыкание:
+Напишите функцию get_advanced_writer(file_name), принимающую название файла для записи.
+Внутри этой функции, напишите ещё одну - write_everything(*data_set), где *data_set - параметр,
+принимающий неограниченное количество данных любого типа.
+Логика write_everything заключается в добавлении в файл file_name всех данных из data_set в том же виде.
+Функция get_advanced_writer возвращает функцию write_everything."""
+
 write = get_advanced_writer('example.txt')
 write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+"""Метод __call__:
+Создайте класс MysticBall, объекты которого обладают атрибутом words хранящий коллекцию строк.
+В этом классе также определите метод __call__ который будет случайным образом выбирать слово из words и возвращать его. 
+Для случайного выбора с одинаковой вероятностью для каждого данного в коллекции функция choice из модуля random.
+"""
 
 import random
 
